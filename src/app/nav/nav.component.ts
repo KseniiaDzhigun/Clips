1280cb3df2f370ef2374f9ecc2e35061c935a3f3
Kseniia Dzhigun <kseniia.balandina@gmail.com>
2023-07-06 22:17:58 +0200

    feat: home, about, manage, logout routes

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -3,6 +3,7 @@ import { ModalService } from '../services/modal.service';
 import { AuthService } from '../services/auth.service';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 
+
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
@@ -13,7 +14,6 @@ export class NavComponent {
   constructor(
     public modal: ModalService,
     public auth: AuthService,
-    private afAuth: AngularFireAuth,
   ) {}
 
   openModal($event: Event) {
@@ -22,9 +22,5 @@ export class NavComponent {
     this.modal.toggleModal('auth')
   }
 
-  async logout($event: Event) {
-    $event.preventDefault()
 
-    await this.afAuth.signOut()
-  }
 }
