6b6462dd943fe5230038d0525832d186dca4a5e2
Kseniia Dzhigun <kseniia.balandina@gmail.com>
2023-07-18 09:27:47 +0200

    feat: NotFound component, guard for the video routes, query parameters in Manage route

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,13 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-manage',
   templateUrl: './manage.component.html',
   styleUrls: ['./manage.component.css']
 })
-export class ManageComponent implements OnInit{
-  constructor() {}
+export class ManageComponent implements OnInit {
+  videoOrder = '1'
 
-  ngOnInit() : void {
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+  ) { }
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params: Params) => {
+      this.videoOrder = params.sort === '2' ? params.sort : '1'
+    })
+  }
+
+  sort(event: Event) {
+    const { value } = (event.target as HTMLSelectElement)
+
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: {
+        sort: value,
+      }
+    })
+
+    // this.router.navigateByUrl(`/manage?sort=${value}`)
   }
 }
